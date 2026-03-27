c0e41e2ec1ede1a2dee836636ca26d3167cbba3c
vector8labs-del <vector8labs@gmail.com>
2026-03-28 00:58:56 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,33 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (typeof window !== "undefined") {
+  !(function (f: any, b: Document, e: string, v: string, n?: any, t?: any, s?: any) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode?.insertBefore(t, s);
+  })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+
+  window.fbq("init", "924125403838932");
+  window.fbq("track", "PageView");
+}
+
+declare global {
+  interface Window {
+    fbq: any;
+    _fbq: any;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
